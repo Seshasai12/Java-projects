be9c5c2f6017a3141ae2877d5ae17f3e360ab207
Seshasai12 <111555815+Seshasai12@users.noreply.github.com>
2022-09-01 12:55:08 +0530

    Add files via upload

---
+++ terrinaryoperator.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class terrinaryoperator {
+public static void main(String args[] ){
+	
+
+int s1=188;//Ternary operator
+String sss=((s1>=200)?"less t 200":"g to 200");
+System.out.println(sss);
+}
+}
---
+++ voteforage.java
@@ -0,0 +1,24 @@
+package operators;
+import constructor.*;
+import java.math.*;
+import java.util.Scanner;
+import java.util.Scanner.*;
+
+public class voteforage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter ur age:");
+int age=s.nextInt();
+if(age>=18) {
+	System.out.println("Vote is Eligible:");}
+	else {
+		System.out.println("Vote is not  Eligible:");
+	}
+		
+}
+	}
+
+
